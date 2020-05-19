c7106f15e0cb32ae96bc4c5b05d690863664361b
Lingyu Zi <48074027+ZiLingyu@users.noreply.github.com>
2020-05-19 11:21:58 -0400

    Add files via upload

---
+++ Parse_Prolog_Code.py
@@ -0,0 +1,27 @@
+import re
+
+
+def remove_matches(regex, file_contents):
+    match = re.search(regex, file_contents, re.MULTILINE)
+    while match is not None:
+        file_contents = file_contents[:match.start()] + file_contents[match.end():]
+        match = re.search(regex, file_contents, re.MULTILINE)
+    return file_contents
+
+
+def parse_file(filename):
+    file_contents = open(filename).read()
+    comment_regex = r"(% *.*)|(\/\*(.|\n)*\*\/)"
+    file_contents = remove_matches(comment_regex, file_contents)
+
+    string_regex = r"\"[^\"]+\""
+    file_contents = remove_matches(string_regex, file_contents)
+
+    identifier_regex = r"[a-zA-Z]+[a-zA-Z_0-9]*"
+    matches = re.finditer(identifier_regex, file_contents)
+    identifiers = set()
+    for match in matches:
+        identifiers.add(file_contents[match.start():match.end()])
+
+    return identifiers
+
